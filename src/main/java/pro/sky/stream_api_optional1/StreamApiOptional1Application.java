4038b2ea594e2c21c893c148c6461b3d032cdb95
mycomputer <Danil12.07.2012@gail.com>
2023-04-13 22:58:03 +0300

    Stream API  optional

---
+++ src/main/java/pro/sky/stream_api_optional1/StreamApiOptional1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.stream_api_optional1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiOptional1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiOptional1Application.class, args);
+    }
+
+}
