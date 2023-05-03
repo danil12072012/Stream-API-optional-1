8e5387207c2f5435a2e740bb3d54d64ecccceb7a
mycomputer <Danil12.07.2012@gail.com>
2023-05-03 21:52:45 +0300

    2.10

---
+++ src/main/java/pro/sky/stream_api_optional1/exeption/IncorrectSurnameExeption.java
@@ -0,0 +1,8 @@
+package pro.sky.stream_api_optional1.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class IncorrectSurnameExeption extends RuntimeException{
+}
